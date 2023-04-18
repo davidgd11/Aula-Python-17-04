561feb2ec46f08a5c2309253c0febc4e04e15b25
David <daviddenunci@gmail.com>
2023-04-18 08:37:16 -0300

    .

--- Aulapy.py
+++ Aulapy.py
@@ -1,4 +1,4 @@
-"""opcao = int(input("Escolha a opção desejada de 1 a 5: "))
+opcao = int(input("Escolha a opção desejada de 1 a 5: "))
 
 match opcao:
     case 1:
@@ -18,25 +18,25 @@ match opcao:
         
     case _:
         print("Opção inválida")
-        """
         
-"""São Paulo - plataforma 1 
+        
+São Paulo - plataforma 1 
 Rio de Janeiro - plataforma 2
 Curitiba - plataforma 3
 Brasília - plataforma 4
-Florianópolis - plataforma 5"""
+Florianópolis - plataforma 5
 
 
-#print("""Olhe aqui os destinos: 
-  #   (1) São Paulo; \n 
-  #   (2) Rio de Janeiro; \n 
- #    (3) Curitiba; \n 
- #    (4) Brasília; \n
- #    (5) Florianópolis. \n """)
-# plataforma = int(input("Qual é o seu destino? (Digite o número de 1 a 5): "))
+print("""Olhe aqui os destinos: 
+     (1) São Paulo; \n 
+     (2) Rio de Janeiro; \n 
+     (3) Curitiba; \n 
+     (4) Brasília; \n
+     (5) Florianópolis. \n """)
+ plataforma = int(input("Qual é o seu destino? (Digite o número de 1 a 5): "))
 
 #Utilizar números para minimizar as chances de erro na hora da digitação
-"""
+
 match plataforma:
     case 1:
         print("Vá para a plataforma 1")
@@ -56,16 +56,16 @@ match plataforma:
     case _:
         print("Opção inválida")
         
-    """    
+            
         
-#print("""Dê uma olha em nosso ótimos vinhos!
-   #   (1) Casa Valduga Gran R$ 160,00. \n
-    #  (2) Casa Silva Gran Terrois Los Carménère R$130,00. \n
-    #  (3) Chocalan Origen Viogner R$ 180,00. \n
-  #    (4) Miolo Demi-sec Reserva especial 2005 R$ 90,00. \n
-    #  (5) Casa Silva Terrois Cabernet Savignon R$ 100,00.\n """)
+print("""Dê uma olha em nosso ótimos vinhos!
+      (1) Casa Valduga Gran R$ 160,00. \n
+      (2) Casa Silva Gran Terrois Los Carménère R$130,00. \n
+      (3) Chocalan Origen Viogner R$ 180,00. \n
+      (4) Miolo Demi-sec Reserva especial 2005 R$ 90,00. \n
+      (5) Casa Silva Terrois Cabernet Savignon R$ 100,00.\n """)
       
-"""    
+   
 vinho = int (input("Qual vinho você deseja escolher? (Digite o número pré estabelecido no menu de opções.): " ))
 
 match vinho:
@@ -86,7 +86,7 @@ match vinho:
     
     case _:
         print("Opção inválida!")
-        """
+        
         
 num1 = float(input("Digite um número: "))
 num2 = float(input("Digite um número: "))
